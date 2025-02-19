7df6b6656a42d4f10386459c6d980ed543f91838
boluwatife-py <boluwatife8081@gmail.com>
2025-02-19 11:38:54 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 
 # Apply migrations
+python3 manage.py makemigrations
 python3 manage.py migrate
 
 # Collect static files
